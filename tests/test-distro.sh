2f2c4a3522bf9a596e406a564833a2cca4055484
John Bresnahan <john.bresnahan@enstratius.com>
2014-02-07 16:09:29 -1000

    Cleaning up the dist test

--- tests/test-distro.sh
+++ tests/test-distro.sh
@@ -12,9 +12,9 @@ mkdir -p $output_dir
 
 which dpkg
 if [ $? -eq 0 ]; then
-    dpkg -i $pkg 
+    dpkg -i $pkg >&1 | tee $output_dir/build.output
 else
-    rpm -i $pkg 
+    rpm -i $pkg >&1 | tee $output_dir/build.output
 fi
 
-
+/opt/es-ex-pyagent/embedded/bin/nosetests -vx --tests dcm.agent.tests.integration.test_reply_receive 2>&1 | tee $output_dir/nosetests.output
