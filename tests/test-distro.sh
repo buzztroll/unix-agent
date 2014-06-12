10481594c513c055474c09866cd21342b8db4a79
John Bresnahan <john@bresnahan.me>
2014-06-12 17:12:32 -0500

    Including the install script in the tests

--- tests/test-distro.sh
+++ tests/test-distro.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-set -e
-
-if [ "X$2" != "X" ]; then
-    export DCM_AGENT_STORAGE_CREDS=$2
+if [ "X$1" != "X" ]; then
+    export DCM_AGENT_STORAGE_CREDS=$1
 fi
 
 DIR=`dirname $0`
@@ -14,23 +12,19 @@ mkdir -p $output_dir
 
 pbase=`hostname`
 echo "selecting the package for $pbase"
-pkg_dir=$1
 
-echo "installing the package $pkg"
-which dpkg
-if [ $? -eq 0 ]; then
-    pkg=`ls -C1 $pkg_dir/*.deb`
-    sudo dpkg -i $pkg >&1 | tee $output_dir/build.output
-else
-    pkg=`ls -C1 $pkg_dir/*.rpm`
-    sudo rpm -i $pkg >&1 | tee $output_dir/build.output
-fi
+export AGENT_BASE_URL=file:////agent/pkgs/
 
 export SYSTEM_CHANGING_TEST=1
-
 echo "running configure"
-sudo /opt/dcm-agent/embedded/bin/dcm-agent-configure --cloud Amazon --url ws://enstratius.com:16309/ws --base-path /dcm
 
-sudo /opt/dcm-agent/embedded/bin/nosetests dcm.agent.tests 2>&1 | tee $output_dir/nosetests.output
+sudo apt-get -y update
+sudo apt-get -y install curl
+sudo yum -y update
+sudo yum -y install curl
+
+sudo -E /agent/bin/installer.sh --cloud Amazon --url ws://enstratius.com:16309/ws --base-path /dcm
+
+sudo -E /opt/dcm-agent/embedded/bin/nosetests dcm.agent.tests 2>&1 | tee $output_dir/nosetests.output
 
 exit 0
