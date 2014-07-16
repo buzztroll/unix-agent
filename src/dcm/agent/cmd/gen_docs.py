21253a54ce12d93e3bf8c350f166bd869cbbf560
Jeremy Phelps <jeremy.phelps@enstratius.com>
2014-07-16 13:23:01 -0500

    gen_docs added as entry point

--- src/dcm/agent/cmd/gen_docs.py
+++ src/dcm/agent/cmd/gen_docs.py
@@ -1,26 +1,40 @@
-
-import os, importlib
+import os
+import importlib
 import dcm.agent.jobs.builtin as jobs
 
-## get files
-filelist = [f for f in os.listdir(os.path.dirname(jobs.__file__)) 
-            if not f.startswith("__init") 
-            and not f.endswith("py") 
+filelist = [f for f in os.listdir(os.path.dirname(jobs.__file__))
+            if not f.startswith("__init")
+            and not f.endswith("py")
             and not f.startswith("gen_doc")
-            ]
+]
 
-
-for f in filelist:
+def dynamic_import(f):
     filename = f[:-4]
     fpath = "dcm.agent.jobs.builtin." + filename
     x = importlib.import_module(fpath)
-    for thing in dir(x):
-        o = getattr(x, thing)
+    return (filename,x)
+
+
+def get_protocol_argument_dict(x):
+
+    for thing in dir(x[1]):
+        o = getattr(x[1], thing)
         z = getattr(o, 'protocol_arguments', None)
+
         if z is not None:
-            print '## ' + filename + ' parameters'
+            print '## ' + x[0] + ' parameters'
+
             for key, value in z.iteritems():
-                print '* ' + key + ': ' + value[0]
-                print '    * optional: ' + '%s' % value[1] 
-                print '    * type: ' + '%s' % value[2] 
+                print '- ' + key + ': ' + value[0]
+                print '    - optional: ' + '%s' % value[1]
+                print '    - type: ' + '%s' % value[2]
                 print ''
+
+
+def main():
+    for f in filelist:
+        x = dynamic_import(f)
+        get_protocol_argument_dict(x)
+
+if __name__ == main():
+    main()
