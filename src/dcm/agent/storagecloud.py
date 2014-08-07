8498b9b8d2465ceb371244b7a3df6c5058f9cfb3
Jeremy Phelps <jeremy.phelps@enstratius.com>
2014-08-06 22:06:32 -0500

    added some documentation

--- src/dcm/agent/storagecloud.py
+++ src/dcm/agent/storagecloud.py
@@ -55,6 +55,17 @@ def _no_driver_function(storage_access_key,
                         endpoint=None,
                         account=None,
                         **kwargs):
+    """
+    This function is called from _map_cloud_name_to_func
+    if cloud_type is not in cloud_map
+    :param storage_access_key:
+    :param storage_secret_key:
+    :param region_id:
+    :param endpoint:
+    :param account:
+    :param kwargs:
+    :raise: AgentUnsupportedCloudFeature is raised
+    """
     raise exceptions.AgentUnsupportedCloudFeature()
 
 
@@ -64,6 +75,17 @@ def _aws_driver_function(storage_access_key,
                          endpoint=None,
                          account=None,
                          **kwargs):
+    """
+    :param storage_access_key: aws access key
+    :param storage_secret_key: aws secret access key
+    :param region_id: example us-east-1 default=None
+    :param endpoint: default=None
+    :param account: default=None
+    :param kwargs:
+    :return: Instance of proper driver class from
+             libcloud.storage.drivers.s3.py based on region_id
+             if no region_id is passed the default is used.
+    """
     region_map = {
         "default": Provider.S3,
         "us_west": Provider.S3_US_WEST,
@@ -89,6 +111,16 @@ def _gce_driver_function(storage_access_key,
                          endpoint=None,
                          account=None,
                          **kwargs):
+    """
+    :param storage_access_key: google access key
+    :param storage_secret_key: google secret access key
+    :param region_id: default=None
+    :param endpoint: default=None
+    :param account: default=None
+    :param kwargs:
+    :return: Instance of driver class from
+             libcloud.storage.drivers.google_storage.py
+    """
     provider = Provider.GOOGLE_STORAGE
     driver_cls = libcloud_providers.get_driver(provider)
     driver = driver_cls(storage_access_key, storage_secret_key)
@@ -101,6 +133,16 @@ def _azure_driver_function(storage_access_key,
                            endpoint=None,
                            account=None,
                            **kwargs):
+    """
+    :param storage_access_key: azure account name
+    :param storage_secret_key: azure account key
+    :param region_id: default=None
+    :param endpoint: default=None
+    :param account: default=None
+    :param kwargs:
+    :return: Instance of driver class from
+             libcloud.storage.drivers.azure_blobs.py
+    """
     provider = Provider.AZURE_BLOBS
     driver_cls = libcloud_providers.get_driver(provider)
     driver = driver_cls(storage_access_key, storage_secret_key)
@@ -113,6 +155,16 @@ def _openstack_driver_function(storage_access_key,
                                endpoint=None,
                                account=None,
                                **kwargs):
+    """
+    :param storage_access_key:
+    :param storage_secret_key:
+    :param region_id:
+    :param endpoint:
+    :param account:
+    :param kwargs:
+    :return: Instance of driver class from
+             libcloud.storage.drivers.cloudfiles.py
+    """
     provider = Provider.OPENSTACK_SWIFT
     driver_cls = libcloud_providers.get_driver(provider)
     driver = driver_cls(storage_access_key, storage_secret_key)
@@ -126,6 +178,21 @@ def _map_cloud_name_to_provider(cloud_type,
                                 endpoint=None,
                                 account=None,
                                 **kwargs):
+    """
+    This function is called by get_cloud_driver and is uses cloud_type and cloud_map
+    to get the correct cloud driver function.  If cloud_type is not in cloud_map then
+    _no_driver_function is called which raises an exception
+
+    :param cloud_type: Name of cloud from dcm.agent.cloudmetadata:CLOUD_TYPES
+                       Ex: CLOUD_TYPES.Amazon -> 'Amazon'
+    :param storage_access_key: cloud specific access key or account name for Azure
+    :param storage_secret_key: cloud specific secret access key
+    :param region_id: default=None
+    :param endpoint: default=None except OpenStack is special
+    :param account: default=None except OpenStack is special
+    :param kwargs:
+    :return:
+    """
     cloud_map = {
         CLOUD_TYPES.Amazon: _aws_driver_function,
         CLOUD_TYPES.Azure: _azure_driver_function,
@@ -231,6 +298,21 @@ def get_cloud_driver(cloud_id,
                      endpoint=None,
                      account=None,
                      **kwargs):
+    """
+    This function is the entry point for this module.  Used to
+    take cloud_id and map it to a cloud_type in order to pass
+    to _map_cloud_name_to_provider.  If a cloud_id is passed which does not
+    exist then an exception is raised.
+
+    :param cloud_id: numerical id of cloud..options in _map_cloud_to_type dict
+    :param access_key: cloud access key
+    :param secret_access_key: cloud secret key
+    :param region_id: default=None
+    :param endpoint: default=None
+    :param account: default=None
+    :param kwargs:
+    :return: cloud specific driver class from _map_cloud_name_to_provider
+    """
     if cloud_id in _map_cloud_id_to_type:
         cloud_type = _map_cloud_id_to_type[cloud_id]
     else:
