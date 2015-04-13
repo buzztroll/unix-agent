6bb5cfe8e3a2d7de1a95d28390349cd9f5f37b9c
John Bresnahan <john@bresnahan.me>
2015-04-13 11:29:37 -1000

    Adding the PATH to the clean env which the agent runs under.

--- bin/installer.sh
+++ bin/installer.sh
@@ -424,7 +424,7 @@ fi
 # Create configuration file and optionally install chef client(subject to change).
 if [ "X$1" == "X" ]; then
     echo /opt/dcm-agent/embedded/bin/dcm-agent-configure -i --base-path /dcm
-    env -i /opt/dcm-agent/embedded/bin/dcm-agent-configure -i --base-path /dcm
+    env -i PATH=$PATH /opt/dcm-agent/embedded/bin/dcm-agent-configure -i --base-path /dcm
     # Install optional packages.
     install_chef_client
 else
@@ -441,7 +441,7 @@ else
         esac
       done
     echo /opt/dcm-agent/embedded/bin/dcm-agent-configure $@
-    env -i /opt/dcm-agent/embedded/bin/dcm-agent-configure $@
+    env -i PATH=$PATH /opt/dcm-agent/embedded/bin/dcm-agent-configure $@
 fi
 
 # Notification for non-native packages.
