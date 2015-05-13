432ca93a3fd6e45422bb93fe5c0945458ce23321
Jeremy Phelps <jeremy.phelps@enstratius.com>
2015-05-13 17:14:50 -0500

    general_cleanup script

---
+++ src/dcm/agent/scripts/common-linux/general_cleanup.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import logging
+
+_g_logger = logging.getLogger(__name__)
+
+
+def main(bin_path, dcm_basedir):
+
+    log_dir = os.path.join(dcm_basedir, 'logs')
+    for (dirpath, dirname, filenames) in os.walk(log_dir):
+        for file in filenames:
+            cmd = '%s %s' % \
+                (os.path.join(bin_path, 'secureDelete'),
+                 os.path.join(dirpath, file))
+            os.system(cmd)
+
+if __name__ == "__main__":
+    try:
+        dcm_basedir = os.environ.get('DCM_BASEDIR')
+    except Exception as ex:
+        _g_logger.exception("general_cleanup failed: " + str(ex.message))
+        sys.exit(1)
+    bin_path = os.path.dirname(os.path.abspath(__file__))
+    main(bin_path, dcm_basedir)
