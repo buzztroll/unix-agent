6b2c4407253e4faf7984120a5050c38bc3c5249f
John Bresnahan <john@bresnahan.me>
2015-05-18 08:00:41 -1000

    Removing unneeded doced out code

--- src/dcm/agent/handshake.py
+++ src/dcm/agent/handshake.py
@@ -28,60 +28,6 @@ import dcm.agent.utils as utils
 _g_logger = logging.getLogger(__name__)
 
 
-# def get_handshake(conf, token):
-#     plugin_dict = jobs.get_all_plugins(conf)
-#     features = conf.features.copy()
-#     for plugin_name in plugin_dict:
-#         p_feature = jobs.get_module_features(
-#             conf, plugin_name, plugin_dict[plugin_name])
-#         features.update(p_feature)
-#
-#     features['plugins'] = get_plugin_handshake_descriptor(conf)
-#
-#     if conf.test_skip_handshake:
-#         # TODO make this configurable from the test conf files
-#         return {
-#             'ipv4': ["127.0.0.1"],
-#             'ipv6': [],
-#             'agent_id': "test-agent",
-#             'vm_instance': "vdeadbeef",
-#             'injected_id': "ideadbeef",
-#             'token': token,
-#             'version': dcm.agent.g_version,
-#             'protocol_version': dcm.agent.g_protocol_version,
-#             'platform': conf.platform_name,
-#             'features': features
-#         }
-#
-#     ipv4s = conf.meta_data_object.get_handshake_ip_address()
-#     ipv6s = []
-#     injected_id = cloudmetadata.get_env_injected_id()
-#     if not injected_id:
-#         _g_logger.debug("No ENV ID set, check the cloud specific "
-#                         "metadata object")
-#         injected_id = conf.meta_data_object.get_injected_id()
-#     agent_id = conf.agent_id
-#     if FOR_TEST_AGENT_ID_ENV in os.environ:
-#         agent_id = os.environ[FOR_TEST_AGENT_ID_ENV]
-#
-#     vm_instance = conf.meta_data_object.get_instance_id()
-#
-#     handshake_doc = {
-#         'ipv4': ipv4s,
-#         'ipv6': ipv6s,
-#         'agent_id': agent_id,
-#         'token': token,
-#         'vm_instance': vm_instance,
-#         'injected_id': injected_id,
-#         'version': dcm.agent.g_version,
-#         'protocol_version': dcm.agent.g_protocol_version,
-#         'platform': conf.platform_name,
-#         'platform_version': conf.platform_version,
-#         'features': features
-#     }
-#     return handshake_doc
-
-
 def get_plugin_handshake_descriptor(conf):
     items = jobs.get_all_plugins(conf)
     command_name_list = [i for i in items]
@@ -144,6 +90,8 @@ class HandshakeManager(object):
         if not os.path.exists(token_dir):
             os.mkdir(token_dir, 0700)
 
+        # At some point we should validate that only this user can read this
+        # file
         # utils.validate_file_permissions(
         #     token_dir, username=self.conf.system_user, permissions=0700)
         #
