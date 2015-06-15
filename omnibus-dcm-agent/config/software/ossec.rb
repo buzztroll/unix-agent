7d193a2fb3d9ef57926979184e5f3f20e5bab207
John Bresnahan <john@bresnahan.me>
2015-06-15 10:08:01 -0500

    Adding ossec to the omnibus build

---
+++ omnibus-dcm-agent/config/software/ossec.rb
@@ -0,0 +1,23 @@
+name "ossec"
+default_version "2.9.0"
+
+# until 2.9 is released we can use the git version
+source git: "https://github.com/ossec/ossec-hids.git"
+
+relative_path "ossec-hids"
+
+build do
+  source_path = "/agent"
+  dst_path = "/opt/ossec"
+  build_env = {
+    "USER_NO_STOP" => "y",
+    "USER_ENABLE_EMAIL" => "n",
+    "USER_DIR" => dst_path,
+    "USER_INSTALL_TYPE" => "local",
+    "USER_LANGUAGE" => "en",
+    "USER_ENABLE_SYSCHECK" => "y",
+    "USER_ENABLE_ROOTCHECK" => "y",
+    "USER_ENABLE_ACTIVE_RESPONSE" => "n"
+  }
+  command "#{source_path}/install.sh", :env => build_env
+end
