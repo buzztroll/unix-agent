746c2ed748373270614ac681cd873dea807945dd
John Bresnahan <john@bresnahan.me>
2015-06-15 17:25:26 -0500

    Setting ossec to version 2.8.2 until 2.9 is released

--- omnibus-dcm-agent/config/software/ossec.rb
+++ omnibus-dcm-agent/config/software/ossec.rb
@@ -1,8 +1,9 @@
 name "ossec"
-default_version "2.9.0"
+default_version "2.8.2"
 
 # until 2.9 is released we can use the git version
-source git: "https://github.com/ossec/ossec-hids.git"
+source url: "http://www.ossec.net/files/ossec-hids-2.8.2.tar.gz",
+       md5: "3036d5babc96216135759338466e1f79"
 
 relative_path "ossec-hids"
 
