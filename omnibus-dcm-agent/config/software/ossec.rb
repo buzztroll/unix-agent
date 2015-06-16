a8bd810b4976b7f51c743efb32f13fea1f06cdee
John Bresnahan <john@bresnahan.me>
2015-06-16 11:10:35 -0500

    Adding new backup tests and patch ossec builder

--- omnibus-dcm-agent/config/software/ossec.rb
+++ omnibus-dcm-agent/config/software/ossec.rb
@@ -5,12 +5,12 @@ default_version "2.8.2"
 source url: "http://www.ossec.net/files/ossec-hids-2.8.2.tar.gz",
        md5: "3036d5babc96216135759338466e1f79"
 
-relative_path "ossec-hids"
+relative_path "ossec-hids-2.8.2"
 
 build do
-  source_path = "/agent"
   dst_path = "/opt/ossec"
   build_env = {
+    "USER_CLEANINSTALL" => "y",
     "USER_NO_STOP" => "y",
     "USER_ENABLE_EMAIL" => "n",
     "USER_DIR" => dst_path,
@@ -20,9 +20,11 @@ build do
     "USER_ENABLE_ROOTCHECK" => "y",
     "USER_ENABLE_ACTIVE_RESPONSE" => "n"
   }
-  command "#{source_path}/install.sh", :env => build_env
+  command "./install.sh", :env => build_env
 
   erb source: "ossec.conf.erb",
       dest: "#{dst_path}/etc/ossec.conf",
-      mode: 0644
+      mode: 0644,
+      vars: { dst_path: dst_path }
+
 end
