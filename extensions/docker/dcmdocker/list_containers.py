4b6fa6e2cf4076a97fb3a8f5d91e96248ddcb1e9
John Bresnahan <john@bresnahan.me>
2015-08-13 08:16:00 -1000

    Removing the old license header

--- extensions/docker/dcmdocker/list_containers.py
+++ extensions/docker/dcmdocker/list_containers.py
@@ -1,16 +1,3 @@
-#  ========= CONFIDENTIAL =========
-#
-#  Copyright (C) 2010-2014 Dell, Inc. - ALL RIGHTS RESERVED
-#
-#  ======================================================================
-#   NOTICE: All information contained herein is, and remains the property
-#   of Dell, Inc. The intellectual and technical concepts contained herein
-#   are proprietary to Dell, Inc. and may be covered by U.S. and Foreign
-#   Patents, patents in process, and are protected by trade secret or
-#   copyright law. Dissemination of this information or reproduction of
-#   this material is strictly forbidden unless prior written permission
-#   is obtained from Dell, Inc.
-#  ======================================================================
 import uuid
 
 import dcm.agent.plugins.api.base as plugin_base
